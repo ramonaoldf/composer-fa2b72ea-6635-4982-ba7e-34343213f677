f9818d3f58b2b52a82be3e9acb06664261c49227
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 12:04:39 -0300

    Vesion: 011.002.001.000

--- scripts/create-postgres.sh
+++ scripts/create-postgres.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
 DB=$1;
-postgres=$(pidof postgres)
+# su postgres -c "dropdb $DB --if-exists"
 
-if [ -z "$postgres" ]
-then
-      # Skip Creating postgres database
-      echo "We didn't find a PID for postgres, skipping \$DB creation"
-else
-    if ! su postgres -c "psql $DB -c '\q' 2>/dev/null"; then
-        su postgres -c "createdb -O homestead '$DB'"
-    fi
+if ! su postgres -c "psql $DB -c '\q' 2>/dev/null"; then
+    su postgres -c "createdb -O homestead '$DB'"
 fi
